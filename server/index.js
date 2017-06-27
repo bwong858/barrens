55b019650cd4fd128b285c5d38463ccf9fda8a58
Mason Raasch <raasch.mason@gmail.com>
2017-06-27 13:29:11 -0700

    require socket io

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const bodyParser = require('body-parser');
 // const db = require('../database');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/areas/barrens', (req, res) => {
-  items.selectAll((err, data) => {
-    err ? res.sendStatus(500) : res.json(data);
-  });
+// app.get('/areas/barrens', (req, res) => {
+//   items.selectAll((err, data) => {
+//     err ? res.sendStatus(500) : res.json(data);
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.send('get request to home page');
 });
+
+
+
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');
