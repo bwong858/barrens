76c6218be102884d317829e855856f72da030c17
Mason Raasch <raasch.mason@gmail.com>
2017-06-27 17:23:16 -0700

    get the socket to work on both client and server

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 // const db = require('../database');
 
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = app.listen(9000, () => {
+  console.log('listening on port 9000!');
+});
+const io = require('socket.io').listen(server);
 
+app.use(express.static(__dirname + '/../node_modules'));
 app.use(express.static(__dirname + '/../client/dist'));
-
 
 // app.get('/areas/barrens', (req, res) => {
 //   items.selectAll((err, data) => {
@@ -16,7 +18,16 @@ app.use(express.static(__dirname + '/../client/dist'));
 // });
 
 app.get('/', (req, res) => {
-  res.send('get request to home page');
+  res.send('server is responding to different paths');
+});
+
+app.get('/api/:lat/:long/:channel', (req, res) => {
+  // figure out which region we're looking in
+  //retrieve all messages that have been tagged with that region
+});
+
+app.get('/api/:region/:channel', (req, res) => {
+  //retrieve all messages that have been tagged with that region
 });
 
 app.get('/channels/barrens', (req, res) => {
@@ -25,9 +36,10 @@ app.get('/channels/barrens', (req, res) => {
   });
 });
 
-
-
-
-app.listen(3000, () => {
-  console.log('listening on port 3000!');
+io.on('connection', (socket) => {
+  console.log('a user has connected');
 });
+
+// app.listen(9000, () => {
+  
+// });
