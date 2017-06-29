db8dbc35c2b2f7528f09df0c35b85edcff893984
Brian Wong <brianwong.se@gmail.com>
2017-06-29 01:20:01 -0700

    Clean up, clean up, everybody, everywhere

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -11,19 +11,18 @@ import { dummyChannels, dummyUsers, dummyMessages } from '../dummyData';
 const fetchInitialMessages = (lat, lon) => {
   const getPos = () =>
     new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+
   return getPos().then(pos => {
     const lat = pos.coords.latitude;
     const lon = pos.coords.longitude;
 
-    return fetch(`http://localhost:8000/api/messages/${lat}/${lon}`, {
-      method: 'GET'
-    }).then(res => res.json());
-  });
+    return fetch(`http://localhost:8000/api/messages/${lat}/${lon}`);
+  }).then(res => res.json());
 };
 
 const MessageBoard = ({ socket, messages, setMessages, sendMessage, updateMessages }) => {
   if (!messages.length) {
-    socket.on('message', message => { // problematic if there are no messages to fetch - will continue to create sockets
+    socket.on('message', message => { // problematic if there are no messages to fetch - will continue to create sockets; however, must create socket exactly once in order to begin populating messages on emit
       updateMessages(message);
     });
     // socket.init('subscribe', 'SF-general');
