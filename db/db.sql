3d25ca56ee362776c92c1afc04b627b5ccc89270
artliou <artyliou@gmail.com>
2017-06-28 17:14:52 -0700

    DB Update

---
+++ db/db.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS barrens;
+CREATE DATABASE barrens;
+
+\c barrens;
+
+CREATE TABLE areas (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR,
+  lat INTEGER,
+  long INTEGER
+);
+
+CREATE TABLE channels (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR,
+  users VARCHAR REFERENCES users (name),
+  areas VARCHAR REFERENCES areas (name)
+);
+
+CREATE TABLE messages )
+  ID SERIAL PRIMARY KEY,
+  user VARCHAR REFERENCES users (id),
+  content TEXT,
+  channel VARCHAR REFERENCES name (name),
+  upvotes SMALLINT,
+  downvotes SMALLINT,
+  lat INTEGER,
+  long INTEGER,
+  area VARCHAR REFERENCES areas (name)
+);
+
+CREATE TABLE users (
+  ID SERIAL PRIMARY KEY,
+  username VARCHAR,
+  points INTEGER
+);
+
+CREATE TABLE events (
+  ID SERIAL PRIMARY KEY,
+  area REFERENCES areas (name),
+  description VARCHAR,
+  link VARCHAR
+);
+-- TBD
+CREATE TABLE users_events (
+  ID SERIAL PRIMARY KEY,
+  users
+  events
+);
+
+CREATE TABLE cookies (
+  ID SERIAL PRIMARY KEY,
+  username VARCHAR,
+  area VARCHAR,
+  stamp TIMESTAMPTZ
+);
