cba6edbc4fdc5369e60ce9f5fa578ac73e1b6ba8
artliou <artyliou@gmail.com>
2017-06-29 16:54:59 -0700

    DB Update

--- db/database.js
+++ db/database.js
@@ -1,6 +1,10 @@
 /***
 Sample for how a database would be created via running query scripts.
-Port: 5432
+Default Port: 5432
+
+Example for Connecting NodeJS to PostgreSQL
+http://nodeexamples.com/2012/09/21/connecting-to-a-postgresql-database-from-node-js-using-the-pg-module/
+
 ***/
 
 const pg = require('pg');
@@ -11,19 +15,12 @@ const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/
 Here we create a new instance of Client to interact with the database and then establish communication with it via the connect() method. We then run a SQL query via the query() method. Finally, communication is closed via the end() method.
 ***/
 
+// Creates a connection to the PG DB
 const client = new pg.Client(connectionString);
+
 client.connect();
 const query = client.query(
   'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+
+// When all rows have been queried or an error occurs, then the query will end.
 query.on('end', () => { client.end(); });
-
-
-/***
-For creating tables that don't exist yet
-client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-
-Geo-location
-// insert into geo_table values (1, '((2,2),(3,4),(3,6),(1,1))');
-// GeomFromText('POLYGON((long1 lat1, long2 lat2, long3 lat3))')
-
-***/
--- db/db.sql
+++ db/db.sql
@@ -1,57 +1,114 @@
+/***
+Barren's Database Table Schema
+6 Schemas
+Another Schema is to be build, but is a lower priority
+
+Dependencies: npm install pg
+
+Documentation & Resources:
+http://postgresguide.com/sql/select.html
+http://postgresguide.com/
+
+Sample Commands/Queries:
+
+View all Tables
+/d
+
+Query
+"SELECT username, points FROM users;"
+
+Filtering Data
+"SELECT content, upvotes
+FROM messages
+WHERE lat >= '2012-01-01'
+  AND long < '2012-02-01'
+ ;"
+
+******* Geo-Location
+Installation: brew install postgis
+OR using the POSTGRES.app
+
+Geo-location Commands
+"SELECT superhero.name
+FROM city, superhero
+WHERE ST_Contains(city.geom, superhero.geom)
+AND city.name = 'Gotham';"
+
+Resources:
+http://postgis.net/
+https://postgis.net/docs/ST_Contains.html
+https://postgis.net/docs/ST_MakePoint.html
+Geo-location
+
+insert into geo_table values (1, '((2,2),(3,4),(3,6),(1,1))');
+GeomFromText('POLYGON((long1 lat1, long2 lat2, long3 lat3))')
+
+***/
+
 DROP DATABASE IF EXISTS barrens;
 CREATE DATABASE barrens;
 
+-- Command to Connect to DB
 \c barrens;
+
+-- Enable PostGIS (includes raster)
+CREATE EXTENSION postgis;
 
 CREATE TABLE areas (
   ID SERIAL PRIMARY KEY,
-  name VARCHAR,
-  lat INTEGER,
-  long INTEGER
+  name VARCHAR NOT NULL,
+  lat DOUBLE PRECISION NOT NULL,
+  long DOUBLE PRECISION NOT NULL
 );
-
-CREATE TABLE channels (
-  ID SERIAL PRIMARY KEY,
-  name VARCHAR,
-  users VARCHAR REFERENCES users (name),
-  areas VARCHAR REFERENCES areas (name)
-);
-
-CREATE TABLE messages )
-  ID SERIAL PRIMARY KEY,
-  user VARCHAR REFERENCES users (id),
-  content TEXT,
-  channel VARCHAR REFERENCES name (name),
-  upvotes SMALLINT,
-  downvotes SMALLINT,
-  lat INTEGER,
-  long INTEGER,
-  area VARCHAR REFERENCES areas (name)
-  stamp TIMESTAMPTZ
-);
-
 CREATE TABLE users (
   ID SERIAL PRIMARY KEY,
-  username VARCHAR,
-  points INTEGER
+  username VARCHAR NOT NULL,
+  points INTEGER,
+  session BOOLEAN NOT NULL,
+  hash VARCHAR NOT NULL,
+  salt VARCHAR UNIQUE
+  -- chkpass is alternative data type, needs ckpass module installed
 );
 
 CREATE TABLE events (
   ID SERIAL PRIMARY KEY,
   area REFERENCES areas (name),
   description VARCHAR,
-  link VARCHAR
-);
--- TBD
-CREATE TABLE users_events (
-  ID SERIAL PRIMARY KEY,
-  users
-  events
+  url VARCHAR
 );
 
-CREATE TABLE cookies (
+CREATE TABLE channels (
   ID SERIAL PRIMARY KEY,
-  username VARCHAR,
+  name VARCHAR NOT NULL,
+  users VARCHAR REFERENCES users (name),
+  areas VARCHAR REFERENCES areas (name)
+);
+
+CREATE TABLE messages (
+  ID SERIAL PRIMARY KEY,
+  user VARCHAR REFERENCES users (id),
+  content TEXT NOT NULL,
+  channel VARCHAR REFERENCES name (name),
+  upvotes SMALLINT,
+  downvotes SMALLINT,
+  lat DOUBLE PRECISION NOT NULL,
+  long DOUBLE PRECISION NOT NULL,
+  area VARCHAR REFERENCES areas (name),
+  stamp TIMESTAMPTZ NOT NULL
+);
+
+-- Table Schema for Authentication
+-- TimeStamp TZ - Data Type that includes time, date, time zone
+CREATE TABLE session (
+  ID SERIAL PRIMARY KEY,
+  username VARCHAR NOT NULL,
   area VARCHAR,
   stamp TIMESTAMPTZ
 );
+
+-- Attendees, Join would be many events to many users
+-- CREATE TABLE users_events (
+--   ID SERIAL PRIMARY KEY,
+--   users
+--   events
+-- );
