e91f2a66ef1c376c9d3072673a55dfb2ac913092
Brian Wong <brianwong.se@gmail.com>
2017-06-29 01:24:57 -0700

    Add janky loading message

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -12,22 +12,26 @@ const fetchInitialMessages = (lat, lon) => {
   const getPos = () =>
     new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
 
-  return getPos().then(pos => {
-    const lat = pos.coords.latitude;
-    const lon = pos.coords.longitude;
+  return getPos()
+    .then(pos => {
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
 
-    return fetch(`http://localhost:8000/api/messages/${lat}/${lon}`);
-  }).then(res => res.json());
+      return fetch(`http://localhost:8000/api/messages/${lat}/${lon}`);
+    })
+    .then(res => res.json());
 };
 
 const MessageBoard = ({ socket, messages, setMessages, sendMessage, updateMessages }) => {
   if (!messages.length) {
-    socket.on('message', message => { // problematic if there are no messages to fetch - will continue to create sockets; however, must create socket exactly once in order to begin populating messages on emit
-      updateMessages(message);
-    });
-    // socket.init('subscribe', 'SF-general');
+    // problematic if there are no messages to fetch - will continue to create sockets; however, must create socket exactly once in order to begin populating messages on emit
+    // if messages state is actually empty, message send MUST trigger re-render in order to ever render the message(s)
     fetchInitialMessages().then(messages => {
       setMessages(messages);
+      socket.on('message', message => {
+        updateMessages(message);
+      });
+      // socket.init('subscribe', 'SF-general');
     });
   }
 
@@ -38,7 +42,8 @@ const MessageBoard = ({ socket, messages, setMessages, sendMessage, updateMessag
         <UserList users={dummyUsers} />
       </div>
       <div className="message-list-container inline-block">
-        <MessageList messages={messages} />
+        <MessageList messages={messages.length ? messages : [{ text:
+        'Loading messages...' }]} />
         <MessageInput socket={socket} sendMessage={sendMessage} />
       </div>
     </div>
