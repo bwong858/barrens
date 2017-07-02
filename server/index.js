2ebf7439f90fcad910367f880ceadfa923fc8fd5
Mason Raasch <raasch.mason@gmail.com>
2017-07-02 16:43:56 -0700

    update documentation for db. change config options in index.js for successful db connection. begin forumulating queries to place users in regions

--- server/index.js
+++ server/index.js
@@ -25,23 +25,45 @@ app.get('/api/messages/:lat/:long', (req, res) => {
   // figure out which region we're loo  king in
   //retrieve all messages that have been tagged with that region
   //retrieve all messages tagged with general'
-  console.log('receiving the initial GET request');
-  //retrieve all messages tagged with general
-  res.json(dummyMessages);
+  console.log(`receiving the initial GET request with coords ${req.params.lat}, ${req.params.long}`);
+  //retrieve all messages tagged with the region corresponding to incoming coords
+  db.query('SELECT * from areas;', null, (err, results) => {
+    if (err) {
+      console.log('err querying pg db:', err);
+      res.sendStatus(500);
+    }
+    console.log('successfully got noe mission', results.rows[0].geom);
+    const MissionNoeRegion = `SELECT ST_Polygon(ST_GeomFromText('LINESTRING(37.7453366 -122.4379927, 37.7481003 -122.415084, 37.76088 -122.4127313, 37.7607018 122.4360408, 37.7453366 -122.4379927)'), 4326))`;
+
+    const aPointInMissionNoe = 'SELECT ST_SetSRID(ST_MakePoint(37.7531416, -122.4260732),4326))';
+    db.query(`SELECT ST_Contains(${MissionNoeRegion}, ${aPointInMissionNoe});`, null, (err, resultsSTContains) => {
+      if (err) {
+        console.log('err executing ST_Contains:', err);
+        res.sendStatus(500);
+      }
+      console.log('results of ST_Contains:', resultsSTContains);
+      res.sendStatus(200);
+    });    
+  });
 });
 
 app.get('/api/:lat/:long/:channel', (req, res) => {
-  //every message will have coords and so we'll check each time
-  //actually this should be don
+  //
 });
 
-
-app.get('/api/:region/:channel', (req, res) => {
-  //retrieve all messages that have been tagged with that region and channel
-});
 
 app.get('/api/region/:lat/:long', (req, res) => {
   //retrieve region name based off lat and long
+  console.log(`receiving the initial GET request with coords ${req.params.lat}, ${req.params.long}`);
+  db.query('SELECT * from areas;', null, (err, results) => {
+    if (err) {
+      console.log('err querying pg db:', err);
+      res.sendStatus(500);
+    }
+    console.log('successfully got noe mission', results.rows[0].geom);
+    const aPointInRegion = 'SELECT ST_SetSRID(ST_MakePoint(37.7531416, -122.4260732),4326))';
+    res.sendStatus(200);
+  });
 });
 
 app.get('/hi', (req, res) => {
