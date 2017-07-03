417829eb4d3d0c363b419e8c6a657058f53cefd5
Mason Raasch <raasch.mason@gmail.com>
2017-07-03 11:02:36 -0700

    include dummy response for user updating endpoint

--- server/index.js
+++ server/index.js
@@ -27,24 +27,24 @@ app.get('/api/messages/:lat/:long', (req, res) => {
   //retrieve all messages tagged with general'
   console.log(`receiving the initial GET request with coords ${req.params.lat}, ${req.params.long}`);
   //retrieve all messages tagged with the region corresponding to incoming coords
-  // db.query('SELECT * from areas;', null, (err, results) => {
-  //   if (err) {  
-  //     console.log('err querying pg db:', err);
-  //     res.sendStatus(500);
-  //   }
-  //   console.log('successfully got noe mission', results.rows[0].geom);
-  //   const MissionNoeRegion = `ST_Polygon(ST_GeomFromText('LINESTRING(37.7453366 -122.4379927, 37.7481003 -122.415084, 37.76088 -122.4127313, 37.7607018 122.4360408, 37.7453366 -122.4379927)'), 4326)`;
+  db.query('SELECT * from areas;', null, (err, results) => {
+    if (err) {  
+      console.log('err querying pg db:', err);
+      res.sendStatus(500);
+    }
+    //console.log('successfully got noe mission', results.rows[0].geom);
+    const MissionNoeRegion = `ST_Polygon(ST_GeomFromText('LINESTRING(37.7453366 -122.4379927, 37.7481003 -122.415084, 37.76088 -122.4127313, 37.7607018 -122.4360408, 37.7453366 -122.4379927)'), 4326)`;
 
-  //   const aPointInMissionNoe = 'ST_SetSRID(ST_MakePoint(37.7531416, -122.4260732),4326)';
-  //   db.query(`SELECT ST_Contains(${MissionNoeRegion}, ${aPointInMissionNoe});`, null, (err, resultsSTContains) => {
-  //     if (err) {
-  //       console.log('err executing ST_Contains:', err);
-  //       res.sendStatus(500);
-  //     }
-  //     console.log('results of ST_Contains:', resultsSTContains);
-  //     res.sendStatus(200);
-  //   });    
-  // });
+    const aPointInMissionNoe = 'ST_SetSRID(ST_MakePoint(37.7531416, -122.4260732),4326)';
+    db.query(`SELECT ST_Contains(${MissionNoeRegion}, ${aPointInMissionNoe});`, null, (err, resultsSTContains) => {
+      if (err) {
+        console.log('err executing ST_Contains:', err);
+        res.sendStatus(500);
+      }
+      console.log('results of ST_Contains:', resultsSTContains);
+      res.sendStatus(200);
+    });    
+  });
   res.json(dummyMessages);
 });
 
@@ -79,6 +79,14 @@ app.post('/api/users/:newUsername', (req, res) => {
   });
 });
 
+
+app.put('/api/users/:username/:lat/:long', (req, res) => {
+  //update the user with name=username with the coords denoting their location
+  //update is likely not the same command as insert into
+  //if update is complicated, have it all come in as a single request 
+  //return a response that contains the type string region name 
+  res.status(201).send('Hack reactor');
+});
 // likely no posting messages route -- everything will happen in the socket
 
 io.sockets.on('connection', (socket) => {
@@ -94,7 +102,13 @@ io.sockets.on('connection', (socket) => {
   socket.on('send', (data) => {
     console.log('received message', data);
     io.sockets.in(data.region).emit('message', data);
-    //db.query(`INSERT INTO messages VALUES (DEFAULT, '${}', '${}', )`)
+    db.query(`INSERT INTO messages VALUES (DEFAULT, null, '${data.text}', null, 0, 0, '${data.region}', null, null);`, null, (err, results) => {
+      if (err) {
+        console.log('err inserting into messages', err);
+      } else {
+        console.log('success inserting into messages');
+      }
+    });
     // io.emit('message', data);
   });
 });
